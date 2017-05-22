91441ae76925277dcc8bab6643db44e1ec11633d
Mike Shiel <mikeshiel@Mikes-MacBook-Pro.local>
2017-05-22 15:58:56 +0200

    corrections

---
+++ app/assets/javascripts/navbar.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){       
+   var scroll_start = 0;
+   var startchange = $('#startchange');
+   var offset = startchange.offset();
+   $(document).scroll(function() { 
+      scroll_start = $(this).scrollTop();
+      if(scroll_start > offset.top) {
+          $('#navbar').css('background-color', '#2E2F31');
+       } else {
+          $('#navbar').css('background-color', 'transparent');
+       }
+   });
+});
