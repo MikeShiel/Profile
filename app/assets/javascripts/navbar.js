4a9d1393635b4185dfb1ee1c2469706ecb956a78
Mike Shiel <mikeshiel@Mikes-MacBook-Pro.local>
2017-05-24 18:00:24 +0200

    js corrected

--- app/assets/javascripts/navbar.js
+++ app/assets/javascripts/navbar.js
@@ -1,13 +1,69 @@
-$(document).ready(function(){       
-   var scroll_start = 0;
-   var startchange = $('#startchange');
-   var offset = startchange.offset();
-   $(document).scroll(function() { 
-      scroll_start = $(this).scrollTop();
-      if(scroll_start > offset.top) {
-          $('#navbar').css('background-color', '#2E2F31');
-       } else {
-          $('#navbar').css('background-color', 'transparent');
-       }
-   });
+$(document).ready(function(){
+  var userHeight = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+  var startFade = userHeight - userHeight/2;
+  var endFade = userHeight - 70;
+  var $navbar = $('#navbar');
+  var st = $(this).scrollTop();
+  if ( st <= startFade ) {
+    $navbar.css({ 'background' : 'rgba(43,45,66,0.0)' });
+  } else if ( st > startFade && st < endFade ) {
+    var transparancy = ((st/endFade - startFade/endFade) / (1 - startFade/endFade));
+    $navbar.css({ 'background' : `rgba(43,45,66,${transparancy})` });
+  } else {
+    $navbar.css({ 'background' : 'rgba(43,45,66,1)' });
+  }
+
+  $(window).on('scroll', function() {
+    var st = $(this).scrollTop();
+    if ( st <= startFade ) {
+      $navbar.css({ 'background' : 'rgba(43,45,66,0.0)' });
+    } else if ( st > startFade && st < endFade ) {
+      var transparancy = ((st/endFade - startFade/endFade) / (1 - startFade/endFade));
+      $navbar.css({ 'background' : `rgba(43,45,66,${transparancy})` });
+    } else {
+      $navbar.css({ 'background' : 'rgba(43,45,66,1)' });
+    }
+  });
+});
+
+
+// menu
+$(document).ready(function(){
+  $('#navbar').on('click', '#toAbout', function() {
+    event.preventDefault();
+    var targetOffset = $('#about').offset().top - 70;
+    $("html, body").animate({scrollTop: targetOffset}, 500);
+  });
+
+  $('#navbar').on('click', '#toCV', function() {
+    event.preventDefault();
+    var targetOffset = $('#CV').offset().top - 70;
+    $("html, body").animate({scrollTop: targetOffset}, 500);
+  });
+
+  $('#navbar').on('click', '#toContact', function() {
+    event.preventDefault();
+    var targetOffset = $('#contact').offset().top - 70;
+    $("html, body").animate({scrollTop: targetOffset}, 500);
+  });
+});
+
+function openMenu() {
+  document.getElementById("dropdown").classList.toggle("show");
+}
+
+$(document).ready(function(){
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  };
 });
